7e45461d3838a10bae5cceb0aef6b7b4c0b6d1a5
GerardSanchezV <gerardsanchezvelasco@gmail.com>
2022-06-07 11:48:26 +0200

    Ej 2 completado

---
+++ UD_35_2/src/app/app.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms'
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'UD_35_2';
+
+  formularioContacto = new FormGroup({
+    nombre: new FormControl('', Validators.required),
+    email: new FormControl('', Validators.required),
+    mensaje: new FormControl('', Validators.required),
+    respuesta: new FormControl('', Validators.required),
+  })
+
+  randomNum() {
+    let numero = Math.floor(Math.random() * 6);
+    return numero;
+  }
+
+  valorA = this.randomNum();
+  valorB = this.randomNum();
+
+  get nombre(){return this.formularioContacto.get('nombre')}
+  get email(){return this.formularioContacto.get('email')}
+  get mensaje(){return this.formularioContacto.get('mensaje')}
+  get respuesta(){return this.formularioContacto.get('mensaje')}
+
+}
